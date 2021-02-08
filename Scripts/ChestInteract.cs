a59498efd72b88b4ff0672f413dafd7097630a5c
Tkejczu <kamilossopl1995@gmail.com>
2021-02-08 20:45:18 +0100

    ADD: 3 entry task scripts

---
+++ Scripts/ChestInteract.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInteract : Interactable
+{
+    public override void Interact()
+    {
+        Debug.Log("Player has interacted with a chest!");
+    }
+}
---
+++ Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float maxDistanceToPlayer = 3f;
+    public GameObject player;
+
+    public virtual void Interact()
+    {
+        Debug.Log("Player has interacted with an object!");
+    }
+
+    public virtual bool CanInteract()
+    {
+        if (player)
+        {
+            return Vector3.Distance(this.transform.position, player.transform.position) <= maxDistanceToPlayer;
+        }
+        else
+            return false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        PlayerInteractionScript playerInteraction = collision.gameObject.GetComponent<PlayerInteractionScript>();
+        
+        if (playerInteraction)
+        {
+            player = collision.gameObject;
+            playerInteraction.SubscribeInteractable(this);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        PlayerInteractionScript playerInteraction = collision.gameObject.GetComponent<PlayerInteractionScript>();
+        
+        if (playerInteraction)
+        {
+            playerInteraction.UnsubscribeInteractable();
+            player = null;
+        }
+    }
+}
